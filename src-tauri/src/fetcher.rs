c3c6eb38fc9ff0d8d021af990291e363d21cbf8e
Minigrim0 <grimauflorent@gmail.com>
2023-08-03 10:13:29 +0200

    [Upd] Separated concerns, fetcher runs outside of the app

---
+++ src-tauri/src/fetcher.rs
@@ -0,0 +1,15 @@
+//!    This executable is used to fetch data from the multiple apis and store
+//!    them in redis for caching purposes.
+//!    The different structures built by the apis are serialized to a json string
+//!    and pushed to redis once per day (Using an external cron to run this executable)
+
+use utils::fetch_weather;
+
+pub mod weather;
+pub mod utils;
+
+#[tokio::main(flavor = "multi_thread")]
+async fn main(){
+    let weather = fetch_weather().await;
+    println!("Weather is {:?}", weather);
+}
