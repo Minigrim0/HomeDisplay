156c862f702bb3981e3306cff3e1a7ade6487475
Minigrim0 <grimauflorent@gmail.com>
2023-08-12 16:48:12 +0200

    [Upd] Small fixes

--- src-tauri/src/api/transports.rs
+++ src-tauri/src/api/transports.rs
@@ -39,7 +39,6 @@ pub fn check_bus_stop(stop_name: String) -> Option<BusStop> {
 }
 
 
-// TODO: Add a check to see if bus stop is already in redis
 pub async fn get_bus_stops() -> Option<Vec<BusStop>> {
     let api_key: String = match var("SL_PLACE_API_KEY") {
         Ok(key) => key,
@@ -64,13 +63,13 @@ pub async fn get_bus_stops() -> Option<Vec<BusStop>> {
             bus_stop_list.split(",").collect::<Vec<&str>>()
         },
         Err(_) => {
-            println!("Missing bus stops, can't define what to fetch (export SL_PLACE_API_KEY)");
+            println!("Missing bus stops, can't define what to fetch (export SL_PLACE_BUS_STOPS)");
             return None;
         }
     };
 
     let mut bus_stops_array: Vec<BusStop> = vec![];
-    let stops = &mut bus_stops_array;
+    let stops: &mut Vec<BusStop> = &mut bus_stops_array;
     for stop in bus_stops.iter() {
         match check_bus_stop(stop.to_string()) {
             Some(place_id) => stops.push(place_id),  // The bus stop is cached in redis
