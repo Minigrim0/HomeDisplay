002e0c71e0dddf8334a28796d18e5d838f4adf59
Minigrim0 <grimauflorent@gmail.com>
2024-03-31 15:47:07 +0200

    [Upd] new structure hierarchy + outlined functionality

--- src-tauri/src/transports/api.rs
+++ src-tauri/src/transports/api.rs
@@ -1,22 +1,128 @@
-use crate::transports::models::StopDepartures;
+use reqwest::Url;
+use serde_derive::{Deserialize, Serialize};
+use std::env::var;
 
-/// Represents an answer from the Realtime API endpoint
+use crate::database;
+use crate::transports::models;
+
+/// An answer from the Realtime API endpoint
 /// https://transport.integration.sl.se/v1/sites/{SiteId}/departures
 #[derive(Serialize, Deserialize, Debug)]
-pub struct RealTidAPI {
-    pub departures: Vec<Departure>,
-    pub stop_deviations: Vec<Deviation>,
+pub struct SiteDepartureAPI {
+    pub departures: Vec<models::Departure>,
+    pub stop_deviations: Vec<models::Deviation>,
+}
+
+/// A site from the API
+#[derive(Serialize, Deserialize, Debug)]
+pub struct SiteAPI {
+    pub id: i32,  // Site id, used to get Realtime departures
+    pub name: String,  // The name of the stop
+    pub lat: f32,
+    pub lon: f32,
+}
+
+/// The site list from the API (cache to avoid querying too much)
+#[derive(Serialize, Deserialize, Debug)]
+pub struct SiteListAPI {
+    sites: Vec<SiteAPI>
+}
+
+impl SiteDepartureAPI {
+    /// Refreshes data by calling the API endpoint
+    /// The refreshed data is sent to the redis database
+    async fn refresh(&self) {
+
+    }
+
+    /// Check the freshness of the data in the redis database
+    /// If above a certain threshold, data is refreshed from the API and the new departures
+    /// Are returned and stored in the database
+    pub async fn get(base_url: &String, stop: &models::Site) -> Option<SiteDepartureAPI> {
+        let url: Url = match Url::parse(
+            &*format!("{}/sites/{}/departures", base_url, stop.site_id)
+        ) {
+            Ok(url) => url,
+            Err(error) => {
+                println!("Could not parse realtid URL, {}", error);
+                return None
+            }
+        };
+
+        let result = match reqwest::get(url).await {
+            Ok(resp) => resp,
+            Err(error) => {
+                println!("Unable to fetch realtid information: {}", error);
+                return None
+            }
+        };
+
+        match result.status() {
+            reqwest::StatusCode::OK => {
+                match result.json::<SiteDepartureAPI>().await {
+                    Ok(timings) => Some(timings),
+                    Err(error) => {
+                        println!("Error while parsing SiteDepartureAPI: {}", error.to_string());
+                        None
+                    }
+                }
+            }
+            reqwest::StatusCode::UNAUTHORIZED => {
+                println!("Got unauthorized response while fetching the Departures, check your API key");
+                None
+            },
+            _ => {
+                println!("An error occured while fecthing departures !");
+                None
+            }
+        }
+    }
 }
 
 
-pub async fn get_all_departures() -> Result<Vec<StopDepartures>, String> {
-    let api_key: String = match var("SL_REALTIME_API_KEY") {
-        Ok(key) => key,
-        Err(_) => {
-            return Err("Missing API key for SL realtid API. Can't fetch departures".to_string());
-        }
-    };
+impl SiteListAPI {
+    /// Gets the site list from the API.
+    /// This is intended to be cached in redis for later use (e.g. getting a site's id for the
+    /// departure API)
+    pub async fn get(base_url: &String) -> Result<SiteListAPI, String> {
+        let url: Url = match Url::parse(format!("{}/sites", base_url).as_str()) {
+            Ok(url) => url,
+            Err(err) => return Err(format!("Could not parse URL: {}", err))
+        };
 
+        let result = match reqwest::get(url).await {
+            Ok(resp) => resp,
+            Err(err) => return Err(format!("Unable to fetch bus Sites, Err: {}", err))
+        };
+
+        let result_status = result.status();
+        let result_body = match result.text().await {
+            Ok(body) => body,
+            Err(err) => return Err(format!("Unable to fetch bus Sites, Err: {}", err))
+        };
+
+        match result_status {
+            reqwest::StatusCode::OK => {
+                match serde_json::from_str::<Vec<SiteAPI>>(&result_body.clone()) {
+                    Ok(data) => Ok(SiteListAPI { sites: data}),
+                    Err(e) => Err(format!("Error while fetching bus sites: {}", e.to_string())),
+                }
+            },
+            reqwest::StatusCode::UNAUTHORIZED => Err(format!("Unauthorized to fetch bus Sites, check the API key")),
+            _ => Err(format!("Uh oh! Something unexpected happened while fetching bus Sites")),
+        }
+    }
+
+    /// Filters the Site list ase on a stop name and returns the first match
+    pub fn filter(&self, stop_name: String) -> Option<&SiteAPI> {
+        let filtered: Vec<&SiteAPI> = self.sites.iter().filter(|s| s.name.contains(stop_name.as_str())).collect();
+        filtered.first().map_or(None, |s| Some(*s))
+    }
+}
+
+
+/// Returns all the departures for all stops stored in the redis database
+pub async fn get_all_departures() -> Result<Vec<models::SiteDepartures>, String> {
     let base_url = match var("SL_REALTIME_ROOT_URL") {
         Ok(url) => url,
         Err(_) => {
@@ -24,25 +130,25 @@ pub async fn get_all_departures() -> Result<Vec<StopDepartures>, String> {
         }
     };
 
-    let mut departures_array: Vec<StopDepartures> = vec![];
+    let mut departures_array: Vec<models::SiteDepartures> = vec![];
     let departures = &mut departures_array;
 
-    match connection::scan_iter("homedisplay:stops:*".to_string()).await {
+    match database::scan_iter("homedisplay:stops:*".to_string()).await {
         Ok(stops) => {
             for stop_key in stops.iter() {
                 // Fetch the serialized BusStop
-                let ser_stop: String = match connection::get_redis_key(stop_key.to_string()).await {
+                let ser_stop: String = match database::get_redis_key(stop_key.to_string()).await {
                     Ok(ser_stop) => ser_stop,
                     Err(_err) => continue
                 };
                 // Deserialize it
-                let stop: BusStop = match serde_json::from_str(ser_stop.as_str()) {
+                let stop: models::Site = match serde_json::from_str(ser_stop.as_str()) {
                     Ok(stop) => stop,
                     Err(error) => return Err(format!("Unable to deserialize bus stops, {}", error))
                 };
 
                 // Fetch departures for this stop
-                let res: RealTidAPI = match RealTidAPI::get(&api_key, &base_url, &stop).await {
+                let res: SiteDepartureAPI = match SiteDepartureAPI::get(&base_url, &stop).await {
                     None => {
                         println!("Got no departure information for stop: {}", stop.name);
                         continue;
@@ -50,7 +156,7 @@ pub async fn get_all_departures() -> Result<Vec<StopDepartures>, String> {
                     Some(information) => information
                 };
 
-                departures.push(StopDepartures { stop, departures: res.response_data });
+                departures.push(models::SiteDepartures { stop, departures: res.departures });
             }
         },
         Err(err) => return Err(err)
--- src-tauri/src/transports/models.rs
+++ src-tauri/src/transports/models.rs
@@ -1,22 +1,12 @@
-use std::string;
-
 use serde_derive::{Deserialize, Serialize};
 use reqwest::Url;
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
-pub struct BusStop {
+pub struct Site {
     pub name: String,
     pub site_id: String,
     pub x: String,
     pub y: String,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-pub struct PlatsUppSlagAPI {
-    pub status_code: i32,
-    pub message: Option<String>,
-    pub execution_time: i32,
-    pub response_data: Vec<BusStop>,
 }
 
 
@@ -33,7 +23,7 @@ pub struct Line {
     pub transport_mode: String,
 }
 
-/// A departure (must be linked to a stop)
+/// A departure (must be linked to a Site)
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Departure {
     pub destination: String,
@@ -41,97 +31,11 @@ pub struct Departure {
     pub line: Line,
 }
 
-/// A stop with all its departures
+/// A Site with all its departures
 #[derive(Serialize, Deserialize, Debug)]
-pub struct StopDepartures {
-    pub stop: BusStop,
+pub struct SiteDepartures {
+    pub stop: Site,
     pub departures: Vec<Departure>
 }
 
 
-impl BusStop {
-    pub async fn get(base_url: &String, bus_stop: &String) -> Result<BusStop, String> {
-        let url: Url = match Url::parse(
-            &*format!("{}/sites/{}/departures",
-                base_url, bus_stop
-            )
-        ) {
-            Ok(url) => url,
-            Err(err) => return Err(format!("Could not parse URL: {}", err))
-        };
-
-        let result = match reqwest::get(url).await {
-            Ok(resp) => resp,
-            Err(err) => return Err(format!("Unable to fetch bus stops, Err: {}", err))
-        };
-
-        let result_status = result.status();
-        let result_body = match result.text().await {
-            Ok(body) => body,
-            Err(err) => return Err(format!("Unable to fetch bus stops, Err: {}", err))
-        };
-
-        match result_status {
-            reqwest::StatusCode::OK => {
-                match serde_json::from_str::<PlatsUppSlagAPI>(&result_body.clone()) {
-                    Ok(data) => match data.response_data.first() {
-                        Some(stop) => Ok((*stop).clone()),
-                        None => Err("".to_string())
-                    },
-                    Err(_err) => match serde_json::from_str::<PlatsUppSlagAPIError>(&result_body) {
-                        Ok(error) => Err(format!("An API error occured while fetching the bus stops: {}", error.message.unwrap())),
-                        Err(err) => Err(format!("An error occured while fetching the bus stops: {}", err.to_string()))
-                    }
-                }
-            },
-            reqwest::StatusCode::UNAUTHORIZED => Err(format!("Unauthorized to fetch bus stops, check the API key")),
-            _ => Err(format!("Uh oh! Something unexpected happened while fetching bus stops")),
-        }
-    }
-}
-
-
-impl RealTidAPI {
-    pub async fn get(api_key: &String, base_url: &String, stop: &BusStop) -> Option<RealTidAPI> {
-        let url: Url = match Url::parse(
-            &*format!(
-                "{}?key={}&siteid={}&timewindow={}",
-                base_url, api_key, stop.site_id, 60
-            )
-        ) {
-            Ok(url) => url,
-            Err(error) => {
-                println!("Could not parse realtid URL, {}", error);
-                return None
-            }
-        };
-
-        let result = match reqwest::get(url).await {
-            Ok(resp) => resp,
-            Err(error) => {
-                println!("Unable to fetch realtid information: {}", error);
-                return None
-            }
-        };
-
-        match result.status() {
-            reqwest::StatusCode::OK => {
-                match result.json::<RealTidAPI>().await {
-                    Ok(timings) => Some(timings),
-                    Err(error) => {
-                        println!("Error while parsing RealTidAPI: {}", error.to_string());
-                        None
-                    }
-                }
-            }
-            reqwest::StatusCode::UNAUTHORIZED => {
-                println!("Got unauthorized response while fetching the Departures, check your API key");
-                None
-            },
-            _ => {
-                println!("An error occured while fecthing departures !");
-                None
-            }
-        }
-    }
-}
