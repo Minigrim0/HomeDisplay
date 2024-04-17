b9250b84755671bc9c3ff0a1a0f8e36d2eadc4a5
Minigrim0 <grimauflorent@gmail.com>
2024-04-17 10:17:44 +0200

    [Upd] transport models

--- src-tauri/src/transports/models.rs
+++ src-tauri/src/transports/models.rs
@@ -9,24 +9,15 @@ pub struct Coordinates {
 }
 
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct Site {
     pub name: String,
     pub id: String,
     pub coord: Coordinates,
-    pub departures: Vec<Departure>,
-    pub timestamp: u64  // Represents the freshness of the data (departures)
 }
 
 
-#[derive(Serialize, Deserialize, Debug)]
-pub struct Deviation {
-    pub importance_level: i32,
-    pub message: String,
-}
-
-
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct Departure {
     pub destination: String,
     pub display: String,
