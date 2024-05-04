eb49261361550d73b224244cba82527c3fdf5e1b
Minigrim0 <grimauflorent@gmail.com>
2024-05-04 10:12:36 +0200

    added frontend glue for tauri commands

---
+++ frontend/public/glue.js
@@ -0,0 +1,34 @@
+const invoke = window.__TAURI__.invoke
+
+/**
+ * Tauri binding for the `get_sites` function.
+ * @returns {Promise<>}
+ */
+export async function getSites() {
+    return await invoke("get_sites");
+}
+
+/**
+ * Tauri binding for the `get_departures` function.
+ * @param {Object} site; The site to get the departures from
+ * @returns {Promise<>}
+ */
+export async function getDepartures(site) {
+    return await invoke("get_departures", {site: site});
+}
+
+/**
+ * Tauri binding for the `get_currency` function.
+ * @returns {Promise<>}
+ */
+export async function getCurrency() {
+    return await invoke("get_currency");
+}
+
+/**
+ * Tauri binding for the `get_weather` function.
+ * @returns {Promise<>}
+ */
+export async function getWeather() {
+    return await invoke("get_weather");
+}
