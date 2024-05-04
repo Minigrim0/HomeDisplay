e4902145ba9786ff368c281b5aed5fb132ddc6f9
Minigrim0 <grimauflorent@gmail.com>
2024-05-04 10:12:23 +0200

    use primitive types as parameters for easier wasm bindings

--- src-tauri/src/transports/commands.rs
+++ src-tauri/src/transports/commands.rs
@@ -12,6 +12,6 @@ pub async fn get_sites() -> Result<Vec<Site>, String> {
 #[tauri::command]
 /// Returns the sites from the database. The list is filtered using elements in the
 /// `SL_PLACE_BUS_STOPS` environment variable.
-pub async fn get_departures(site: Site) -> Result<Vec<Departure>, String> {
-    database::get_departures(site).await
+pub async fn get_departures(site_id: String) -> Result<Vec<Departure>, String> {
+    database::get_departures(site_id).await
 }
