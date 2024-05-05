2de31a69f7d423dc48984d4a1b10c2c0ee377cb4
Minigrim0 <grimauflorent@gmail.com>
2024-05-05 13:57:22 +0200

    tried rendering weather component

--- frontend/src/weather/services.rs
+++ frontend/src/weather/services.rs
@@ -1,23 +1,26 @@
 use std::time::Duration;
+use common::models::weather::WeatherInfo;
 use wasm_bindgen_futures::spawn_local;
-use yew::{AttrValue, Callback};
+use yew::Callback;
 use yew::platform::time::sleep;
-
 use crate::glue::get_weather;
 
 const ONE_SEC: Duration = Duration::from_secs(1);
 const WEATHER_REFRESH_INTERVAL: Duration = Duration::from_secs(60);
 
 
-pub fn start_weather_job(weather_ready_cb: Callback<AttrValue>) {
-    // Spawn a background task that will fetch a joke and send it to the component.
+pub fn start_weather_job(weather_ready_cb: Callback<Result<WeatherInfo, String>>) {
+    // Spawn a new task that will fetch the weather every 60 seconds
     spawn_local(async move {
         loop {
-            // Fetch the online joke
+            // Fetch the weather
             let weather = get_weather().await.unwrap();
+        
+            // Convert this JsValue object to a WeatherInfo struct
+            let weather: Result<WeatherInfo, String> = serde_wasm_bindgen::from_value(weather).map_err(|e| e.to_string());
 
             // Emit it to the component
-            weather_ready_cb.emit(AttrValue::from(weather.as_string().unwrap()));
+            weather_ready_cb.emit(weather);
             sleep(WEATHER_REFRESH_INTERVAL).await;
         }
     });
