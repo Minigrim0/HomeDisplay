59ac735542fab6badda1c20516da08f9598ef2d9
Minigrim0 <grimauflorent@gmail.com>
2024-08-04 17:41:30 +0200

    Started laying out weather information

--- tui/src/widget.rs
+++ tui/src/widget.rs
@@ -1,3 +1,4 @@
+use chrono::prelude::{Local, DateTime, Timelike};
 use ratatui::{
     buffer::Buffer,
     layout::{Alignment, Rect},
@@ -33,13 +34,53 @@ impl Widget for &App {
             )
             .border_set(border::THICK);
 
-        let counter_text = Text::from(vec![Line::from(vec![
-            "Weather: ".into(),
-            match &self.weather.weather {
-                Ok(_) => "Some".yellow(),
-                Err(e) => e.as_str().red()
-            },
-        ])]);
+        let counter_text = match &self.weather.weather {
+            Ok(weather) => {
+                let sunrise = {
+                    let timestamp = weather.sys.sunrise;
+                    let sunrise = DateTime::from_timestamp(timestamp, 0).unwrap().with_timezone(&Local);
+                    format!("{:02}:{:02}", sunrise.hour(), sunrise.minute())
+                };
+                let sunset = {
+                    let timestamp = weather.sys.sunset;
+                    let sunset = DateTime::from_timestamp(timestamp, 0).unwrap().with_timezone(&Local);
+                    format!("{:02}:{:02}", sunset.hour(), sunset.minute())
+                };
+
+                Text::from(
+                vec![
+                    Line::from(
+                        format!("Temperature: {}°C", weather.main.temp).bold()
+                    ),
+                    Line::from(
+                        format!("Feels like: {}°C", weather.main.feels_like.to_string().yellow())
+                    ),
+                    Line::from(
+                        format!("High: {}°C", weather.main.feels_like.to_string().yellow())
+                    ),
+                    Line::from(
+                        format!("Low: {}°C", weather.main.feels_like.to_string().yellow())
+                    ),
+
+                    Line::from(
+                        format!("Clouds ?: {}°C", weather.main.feels_like.to_string().yellow())
+                    ),
+
+                    Line::from("🌕 Day time ☀️".to_string().yellow()),
+                    Line::from(format!("🌅 {} 🌄 {}", sunrise, sunset))
+                ]
+            )},
+            Err(e) => Text::from(
+                vec![
+                    Line::from(
+                        "Error !".red().bold(),
+                    ),
+                    Line::from(
+                        e.to_string().yellow(),
+                    )
+                ]
+            )
+        };
 
         Paragraph::new(counter_text)
             .centered()
