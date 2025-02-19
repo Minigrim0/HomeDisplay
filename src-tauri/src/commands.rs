d6247507ef711fc0ef900df254fbf57cf77dbe12
Minigrim0 <grimauflorent@gmail.com>
2025-02-19 19:30:12 +0100

    updated tauri to use settings file

--- src-tauri/src/commands.rs
+++ src-tauri/src/commands.rs
@@ -1,5 +1,9 @@
 use log::trace;
 
+use tauri::State;
+use std::sync::Mutex;
+
+use common::settings::Settings;
 use common::models::weather::WeatherInfo;
 use common::models::transports::{Site, Departure};
 use common::models::currency::Conversion;
@@ -7,29 +11,59 @@ use common::models::currency::Conversion;
 
 #[tauri::command]
 /// Get the current currency conversion from the database.
-pub async fn get_currency() -> Result<Conversion, String> {
+pub async fn get_currency(settings: State<'_, Mutex<Settings>>) -> Result<Conversion, String> {
     trace!("Currency tauri command invoked");
-    common::currency::database::fetch_current_conversion().await
+    let (currency_settings, redis_data) = {
+        let settings = match settings.lock() {
+            Ok(s) => s,
+            Err(e) => return Err(e.to_string()),
+        };
+        (settings.currency.clone(), settings.redis.clone())
+    };
+
+    common::currency::database::fetch_current_conversion(currency_settings, &redis_data).await
 }
 
 #[tauri::command]
 /// Returns the sites from the database. The list is filtered using elements in the
 /// `SL_PLACE_BUS_STOPS` environment variable.
-pub async fn get_sites() -> Result<Vec<Site>, String> {
+pub async fn get_sites(settings: State<'_, Mutex<Settings>>) -> Result<Vec<Site>, String> {
     trace!("Sites tauri command invoked");
-    common::transports::database::get_sites().await
+    let (stops, redis_data) = {
+        let settings = match settings.lock() {
+            Ok(s) => s,
+            Err(e) => return Err(e.to_string()),
+        };
+        (settings.transports.clone(), settings.redis.clone())
+    };
+    common::transports::database::get_sites(stops, &redis_data).await
 }
 
 #[tauri::command]
 /// Returns the sites from the database. The list is filtered using elements in the
 /// `SL_PLACE_BUS_STOPS` environment variable.
-pub async fn get_departures(site_id: String) -> Result<Vec<Departure>, String> {
+pub async fn get_departures(settings: State<'_, Mutex<Settings>>, site_id: String) -> Result<Vec<Departure>, String> {
     trace!("Departures tauri command invoked");
-    common::transports::database::get_departures(site_id).await
+    let redis_data = {
+        let settings = match settings.lock() {
+            Ok(s) => s,
+            Err(e) => return Err(e.to_string()),
+        };
+        settings.redis.clone()
+    };
+    common::transports::database::get_departures(site_id, &redis_data).await
 }
 
 #[tauri::command]
-pub async fn get_weather() -> Result<WeatherInfo, String> {
+pub async fn get_weather(settings: State<'_, Mutex<Settings>>) -> Result<WeatherInfo, String> {
     trace!("Weather tauri command invoked");
-    common::weather::database::fetch_current_weather().await
+    let (weather_settings, redis_data) = {
+        let settings = match settings.lock() {
+            Ok(s) => s,
+            Err(e) => return Err(e.to_string()),
+        };
+        (settings.weather.clone(), settings.redis.clone())
+    };
+
+    common::weather::database::fetch_current_weather(weather_settings, &redis_data).await
 }
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,12 +1,41 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use std::sync::Mutex;
+
+use log::error;
+use clap::Parser;
+use tauri::{Builder, Manager};
+use common::settings::Settings;
+
 mod commands;
 
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[clap(author="Minigrim0", version, about="The Tauri version of home-display")]
+struct Args {
+    #[arg(short = 's', long, default_value = "settings.toml")]
+    /// Path to the settings file
+    settings: String,
+}
 
 fn main() {
     env_logger::init();
-    tauri::Builder::default()
+    let args = Args::parse();
+
+    let settings = match Settings::load_from_file(&args.settings) {
+        Ok(settings) => settings,
+        Err(err) => {
+            error!("Failed to load settings: {}", err);
+            std::process::exit(1);
+        }
+    };
+
+    Builder::default()
+        .setup(|app| {
+              app.manage(Mutex::new(settings));
+              Ok(())
+            })
         .invoke_handler(tauri::generate_handler![
             commands::get_currency,
             commands::get_weather,
