1c432c65a7f6350c84ce32b5661a55cb2659e54d
Minigrim0 <grimauflorent@gmail.com>
2025-07-13 09:59:31 +0200

    Export error module in homedisplay crate
    - Add error module to lib.rs exports
    - Make HomeDisplayError available for dependent crates
    - Complete error system integration

---
+++ homedisplay/src/lib.rs
@@ -0,0 +1,14 @@
+pub mod models;
+pub mod traits;
+pub mod error;
+
+pub mod settings;
+
+#[cfg(feature = "network")]
+pub mod database;
+#[cfg(feature = "network")]
+pub mod currency;
+#[cfg(feature = "network")]
+pub mod transports;
+#[cfg(feature = "network")]
+pub mod weather;
